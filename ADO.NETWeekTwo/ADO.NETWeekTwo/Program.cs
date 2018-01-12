c8a9b9e8f1f89d7054c8fb849bf694c68f2bcf08
1995saba <sabina.orunbayeva@gmail.com>
2018-01-12 18:04:03 +0600

    12.01.2018

---
+++ ADO.NETWeekTwo/ADO.NETWeekTwo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NETWeekTwo
+{
+    // по id заказа узнать сотрудника и его другие заказы
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager
+                  .ConnectionStrings["MyConnectionString"]
+                  .ToString();
+            DataSet dataSet = new DataSet();
+
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string ordersSelectCommand = "SELECT * FROM Orders";
+                sqlConnection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(ordersSelectCommand, connectionString))
+                {
+                    adapter.Fill(dataSet, "Orders");
+                }
+                sqlConnection.Close();
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string employeesSelectCommand = "SELECT * FROM Employees";
+                sqlConnection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(employeesSelectCommand, connectionString))
+                {
+                    adapter.Fill(dataSet, "Employees");
+                }
+                sqlConnection.Close();
+            }
+
+            Console.WriteLine("Enter OrderId:");
+            string orderId = Console.ReadLine();
+            string employeeId = "";
+            foreach (var item in dataSet.Tables["Orders"].Rows)
+            {
+                if ((item as DataRow)["OrderID"].ToString() == orderId)
+                {
+                    employeeId = (item as DataRow)["EmployeeID"].ToString();
+                }
+            }
+
+            foreach (DataRow item in dataSet.Tables["Employees"].Rows)
+            {
+                if (item["EmployeeID"].ToString() == employeeId)
+                {
+                    Console.WriteLine("Employee Name: "+item["FirstName"] +' '+item["LastName"]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Orders: ");
+            foreach (DataRow item in dataSet.Tables["Orders"].Rows)
+            {
+                if (item["EmployeeID"].ToString() == employeeId)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("OrderID: "+item["OrderID"]);
+                    Console.WriteLine("OrderDate: "+item["OrderDate"]);
+                    Console.WriteLine("ShipCountry: "+item["ShipCountry"]);
+                    Console.WriteLine("ShipCity: "+item["ShipCity"]);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADO.NETWeekTwo/TaskTwo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NETWeekTwo
+{
+    class Program
+    {
+        public void GetEmployeesStats(DateTime startDate, DateTime finishDate)
+        {
+            string connectionString = ConfigurationManager
+                  .ConnectionStrings["MyConnectionString"]
+                  .ToString();
+
+            DataSet dataSet = new DataSet();
+            string startDateAsString = $"{startDate.Year}-{startDate.Day}-{startDate.Month}";
+            string finishDateAsString = $"{finishDate.Year}-{finishDate.Day}-{finishDate.Month}";
+
+            string query = "SELECT Orders.OrderID, Orders.EmployeeID, Employees.FirstName, Employees.LastName, [Order Details].UnitPrice " +
+                "FROM Orders" +
+                "LEFT JOIN[Order Details] ON" +
+                "Orders.OrderID =[Order Details].OrderID" +
+                "LEFT JOIN Employees ON" +
+                "Orders.EmployeeID = Employees.EmployeeID" +
+                $"WHERE Orders.OrderDate BETWEEN '{startDateAsString}'  AND  '{finishDateAsString}'";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connectionString))
+                {
+                    adapter.Fill(dataSet, "Stats");
+                }
+                sqlConnection.Close();
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
